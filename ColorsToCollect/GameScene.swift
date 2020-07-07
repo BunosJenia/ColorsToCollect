cd5f62687b5f6ca2a089fd2b912200c3f1cbf37a
Evgeny Bunos <evgeny.bunos@wrenkitchens.com>
2020-07-07 15:48:49 +0300

    mplementation

--- ColorsToCollect/GameScene.swift
+++ ColorsToCollect/GameScene.swift
@@ -9,81 +9,299 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+var touchLocation = CGPoint()
+var offBlackColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+var offWhiteColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0)
+var orangeColor = UIColor.orange
+var blueColor = UIColor(red: 0.2, green: 0.5, blue: 0.9, alpha: 1.0)
+
+var colorSelection = 0
+
+var player = SKSpriteNode()
+var fallingBlock = SKSpriteNode()
+
+var labelMain = SKLabelNode()
+var labelScore = SKLabelNode()
+
+var playerSize = CGSize(width: 60, height: 60)
+var fallingBlockSize = CGSize(width: 40, height: 40)
+
+var fallingBlockSpeed = 3.0
+var fallingBlockRotationSpeed = 0.1
+var spawnTimeFallingBlock = 2.0
+
+var score = 0
+
+var isAlive = true
+
+struct physicsCategory {
+    static let player: UInt32 = 1
+    static let fallingBlock: UInt32 = 2
+}
+
+class GameScene: SKScene,SKPhysicsContactDelegate {
     override func didMove(to view: SKView) {
+        self.backgroundColor = offBlackColor
+        physicsWorld.contactDelegate = self
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        resetGameVariablesOnStart()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        spawnLabelMain()
+        spawnLabelScore()
+        spawnPlayer()
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        timerFallingBlock()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            touchLocation = touch.location(in: self)
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            touchLocation = touch.location(in: self)
+
+            if isAlive == true {
+               player.position.x = touchLocation.x
+            }
+           
+            movePlayerOffScreen()
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch in touches {
+            touchLocation = touch.location(in: self)
+            
+            colorChangingLogic()
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        movePlayerOffScreen()
+        keepPlayerInPosition()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+        
+        if (firstBody.categoryBitMask == physicsCategory.player) && (secondBody.categoryBitMask == physicsCategory.fallingBlock) || (firstBody.categoryBitMask == physicsCategory.fallingBlock) && (secondBody.categoryBitMask == physicsCategory.player) {
+            playerFallingBlockCollision(contactA: firstBody.node as! SKSpriteNode, contactB: secondBody.node as! SKSpriteNode)
+        }
+    }
+    
+    func playerFallingBlockCollision(contactA: SKSpriteNode, contactB: SKSpriteNode) {
+        if contactA.name == "playerName" {
+            if colorSelection == 0 && contactB.name == "fallingBlock0" {
+                contactB.removeFromParent()
+                score += 1
+                updateScore()
+            } else if (colorSelection == 1 || colorSelection == 2) && contactB.name == "fallingBlock0" {
+                gameOverLogic()
+            }
+            
+            if colorSelection == 1 && contactB.name == "fallingBlock1" {
+                contactB.removeFromParent()
+                score += 1
+                updateScore()
+            } else if (colorSelection == 0 || colorSelection == 2) && contactB.name == "fallingBlock1" {
+                gameOverLogic()
+            }
+            
+            if colorSelection == 2 && contactB.name == "fallingBlock2" {
+                contactB.removeFromParent()
+                score += 1
+                updateScore()
+            } else if (colorSelection == 1 || colorSelection == 0) && contactB.name == "fallingBlock2" {
+                gameOverLogic()
+            }
+        }
+        
+        if contactB.name == "playerName" {
+            if colorSelection == 0 && contactA.name == "fallingBlock0" {
+                contactB.removeFromParent()
+                score += 1
+                updateScore()
+            } else if (colorSelection == 1 || colorSelection == 2) && contactA.name == "fallingBlock0" {
+                gameOverLogic()
+            }
+            
+            if colorSelection == 1 && contactA.name == "fallingBlock1" {
+                contactB.removeFromParent()
+                score += 1
+                updateScore()
+            } else if (colorSelection == 0 || colorSelection == 2) && contactA.name == "fallingBlock1" {
+                gameOverLogic()
+            }
+            
+            if colorSelection == 2 && contactA.name == "fallingBlock2" {
+                contactB.removeFromParent()
+                score += 1
+                updateScore()
+            } else if (colorSelection == 1 || colorSelection == 0) && contactA.name == "fallingBlock2" {
+                gameOverLogic()
+            }
+        }
+    }
+    
+    func spawnLabelMain() {
+        labelMain = SKLabelNode(fontNamed: "Futura")
+        labelMain.fontSize = 100
+        labelMain.fontColor = offWhiteColor
+        labelMain.position = CGPoint(x: 0, y: self.frame.size.height/2 - 200)
+        labelMain.text = "Start"
+        
+        self.addChild(labelMain)
+    }
+    
+    func spawnLabelScore() {
+        labelScore = SKLabelNode(fontNamed: "Futura")
+        labelScore.fontSize = 50
+        labelScore.fontColor = offWhiteColor
+        labelScore.position = CGPoint(x: 0, y: -self.frame.size.height/2 + 100)
+        labelScore.text = "Score: \(score)"
+        
+        self.addChild(labelScore)
+    }
+    
+    func spawnPlayer() {
+        player = SKSpriteNode(color: offWhiteColor, size: playerSize)
+        player.size = playerSize
+        
+        player.position = CGPoint(x: 0, y: -self.frame.size.height/2 + 180)
+        
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.affectedByGravity = false
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.categoryBitMask = physicsCategory.player
+        player.physicsBody?.contactTestBitMask = physicsCategory.fallingBlock
+        player.physicsBody?.isDynamic = true
+        
+        player.name = "playerName"
+        
+        self.addChild(player)
+    }
+    
+    func spawnFallingBlock() {
+        let maxPossibleX = Int(self.frame.size.width/2) - 50
+        let randomX = Int.random(in: -maxPossibleX ..< maxPossibleX)
+        let colorOfBlock = Int.random(in: 0..<3)
+        
+        fallingBlock = SKSpriteNode(color: offWhiteColor, size: fallingBlockSize)
+        fallingBlock.position = CGPoint(x: randomX, y: 600)
+        
+        fallingBlock.physicsBody = SKPhysicsBody(rectangleOf: fallingBlock.size)
+        fallingBlock.physicsBody?.affectedByGravity = false
+        fallingBlock.physicsBody?.allowsRotation = false
+        fallingBlock.physicsBody?.categoryBitMask = physicsCategory.fallingBlock
+        fallingBlock.physicsBody?.contactTestBitMask = physicsCategory.player
+        fallingBlock.physicsBody?.isDynamic = true
+        
+        if colorOfBlock == 0 {
+            fallingBlock.color = offWhiteColor
+            fallingBlock.name = "fallingBlock0"
+        }
+        
+        if colorOfBlock == 1 {
+            fallingBlock.color = orangeColor
+            fallingBlock.name = "fallingBlock1"
+        }
+        
+        if colorOfBlock == 2 {
+            fallingBlock.color = blueColor
+            fallingBlock.name = "fallingBlock2"
+        }
+        
+        moveFallingBlockToBottom()
+        
+        self.addChild(fallingBlock)
+    }
+    
+    func moveFallingBlockToBottom() {
+        let moveFoward = SKAction.moveTo(y: -1500, duration: fallingBlockSpeed)
+        let rotateAnimate = SKAction.rotate(byAngle: 0.1, duration: fallingBlockRotationSpeed)
+        let destroy = SKAction.removeFromParent()
+        
+        let sequence = SKAction.sequence([moveFoward, destroy])
+        
+        fallingBlock.run(SKAction.repeatForever(sequence))
+        fallingBlock.run(SKAction.repeatForever(rotateAnimate))
+    }
+    
+    func timerFallingBlock() {
+        let wait = SKAction.wait(forDuration: spawnTimeFallingBlock)
+    
+        let spawn = SKAction.run {
+            if isAlive {
+                self.spawnFallingBlock()
+            }
+        }
+        
+        let sequence = SKAction.sequence([wait, spawn])
+        self.run(SKAction.repeatForever(sequence))
+    }
+    
+    func movePlayerOffScreen() {
+        if isAlive == false {
+            player.position.x = -5000
+        }
+    }
+    
+    func keepPlayerInPosition() {
+        player.position.y = -self.frame.size.height/2 + 180
+    }
+    
+    func colorChangingLogic() {
+        colorSelection = colorSelection + 1
+        
+        if colorSelection == 3 {
+            colorSelection = 0
+        }
+        
+        if colorSelection == 0 {
+            player.color = offWhiteColor
+        }
+        
+        if colorSelection == 1 {
+            player.color = orangeColor
+        }
+        
+        if colorSelection == 2 {
+            player.color = blueColor
+        }
+    }
+    
+    func updateScore() {
+        labelScore.text = "Score: \(score)"
+    }
+    
+    func gameOverLogic() {
+        isAlive = false
+        labelMain.text = "Game Over"
+        
+        waitThenRestartTheGame()
+    }
+    
+    func waitThenRestartTheGame() {
+        let wait = SKAction.wait(forDuration: 3)
+        let theGameScene = GameScene(fileNamed: "GameScene")
+        let theTransition = SKTransition.crossFade(withDuration: 1.0)
+        
+        theGameScene?.scaleMode = SKSceneScaleMode.aspectFill
+        
+        let changeScene = SKAction.run {
+            self.view?.presentScene(theGameScene!, transition: theTransition)
+        }
+        
+        let sequence = SKAction.sequence([wait, changeScene])
+        self.run(SKAction.repeat(sequence, count: 1))
+    }
+    
+    func resetGameVariablesOnStart() {
+        score = 0
+        isAlive = true
     }
 }
